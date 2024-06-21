616ef61ec8fb6f897412f5b753ee3939fc7e4680
Pk Patel <46714886+The5cheduler@users.noreply.github.com>
2024-06-21 17:00:20 -0400

    - updated the spinner and git url

--- install.sh
+++ install.sh
@@ -3,16 +3,18 @@
 # Spinner
 spinner() {
   local pid=$1
-  local delay=0.75
+  local delay=0.1
   local spinstr='|/-\'
+  tput civis # Hide cursor
+  echo -n " "
   while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-    local temp=${spinstr#?}
-    printf " [%c]  " "$spinstr"
-    local spinstr=$temp${spinstr%"$temp"}
-    sleep $delay
-    printf "\b\b\b\b\b\b"
+    for i in $(seq 0 3); do
+      printf "\b${spinstr:$i:1}"
+      sleep $delay
+    done
   done
-  printf "    \b\b\b\b"
+  printf "\b"
+  tput cnorm # Show cursor
 }
 
 # Check if Git is installed
@@ -23,7 +25,7 @@ fi
 
 # Clone the Git repository to the user's home directory
 echo "Installing gitSync..."
-git clone https://github.com/yourusername/gitSync.git $HOME/.gitSync &
+git clone https://github.com/The5cheduler/gitSync.git $HOME/.gitSync &
 spinner $!
 
 # Add the directory to the PATH in the user's shell profile
